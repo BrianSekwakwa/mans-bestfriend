d2e95dabc38e848df887191baba7e0cc8a3f0781
Brian Sekwakwa <briansenpai14@gmail.com>
2019-05-24 14:43:37 +0200

    initial project setup files

---
+++ src/components/Main.js
@@ -0,0 +1,13 @@
+import React, { Component } from "react";
+
+class Main extends Component {
+  render() {
+    return (
+      <div>
+        <div>This is the main</div>
+      </div>
+    );
+  }
+}
+
+export default Main;
