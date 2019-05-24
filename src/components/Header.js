e10c4f93b72fee3e84619dec17b4db794c71e3a1
Brian Sekwakwa <briansenpai14@gmail.com>
2019-05-24 16:10:18 +0200

    added the markup and styling for the app

--- src/components/Header.js
+++ src/components/Header.js
@@ -2,8 +2,12 @@ import React from "react";
 
 function Header() {
   return (
-    <div>
-      <div>Header</div>
+    <div className="container">
+      <header className="header">
+        <h1>Man's Bestfriend</h1>
+        <p>A collection of pictures of adorable dogs</p>
+      </header>
+      <div className="border" />
     </div>
   );
 }
--- src/components/Main.js
+++ src/components/Main.js
@@ -3,8 +3,22 @@ import React, { Component } from "react";
 class Main extends Component {
   render() {
     return (
-      <div>
-        <div>This is the main</div>
+      <div className="container">
+        <form className="form">
+          <select name="breeds">
+            <option value="golden">golden</option>
+            <option value="ice">ice</option>
+            <option value="spice">spice</option>
+            <option value="nice">nice</option>
+          </select>
+          <select name="imageNumber">
+            <option value="5">5</option>
+            <option value="10">10</option>
+            <option value="15">15</option>
+            <option value="20">20</option>
+          </select>
+          <button>Generate</button>
+        </form>
       </div>
     );
   }
