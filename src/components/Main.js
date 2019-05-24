a7928813fab65e71907452fa1f739286fadf3701
Brian Sekwakwa <briansenpai14@gmail.com>
2019-05-24 17:04:25 +0200

    added the state with breeds into the select tag

--- src/components/Main.js
+++ src/components/Main.js
@@ -1,16 +1,37 @@
 import React, { Component } from "react";
 
 class Main extends Component {
+  state = {
+    breeds: ""
+  };
+
+  componentDidMount() {
+    fetch("https://dog.ceo/api/breeds/list/all")
+      .then(res => res.json())
+      .then(data => {
+        this.setState({
+          breeds: Object.keys(data.message)
+        });
+      });
+  }
   render() {
+    let options;
+    const { breeds } = this.state;
+    if (breeds !== "") {
+      options = (
+        <select name="breeds">
+          {breeds.map(breed => (
+            <option key={breed} value={breed}>
+              {breed}
+            </option>
+          ))}
+        </select>
+      );
+    }
     return (
       <div className="container">
         <form className="form">
-          <select name="breeds">
-            <option value="golden">golden</option>
-            <option value="ice">ice</option>
-            <option value="spice">spice</option>
-            <option value="nice">nice</option>
-          </select>
+          {options}
           <select name="imageNumber">
             <option value="5">5</option>
             <option value="10">10</option>
