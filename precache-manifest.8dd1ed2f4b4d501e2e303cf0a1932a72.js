65a9b767481626c60e4dd12a0aba6e7d1106de39
Brian Sekwakwa <briansenpai14@gmail.com>
2019-05-26 11:27:45 +0200

    Updates

---
+++ precache-manifest.8dd1ed2f4b4d501e2e303cf0a1932a72.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "35455c8262fa67ef7ee007e220a711e7",
+    "url": "/mans-bestfriend/index.html"
+  },
+  {
+    "revision": "a427b0aeea9b804d467a",
+    "url": "/mans-bestfriend/static/css/main.a2cf47a6.chunk.css"
+  },
+  {
+    "revision": "681b51057d74cfa8975d",
+    "url": "/mans-bestfriend/static/js/2.65aa1cca.chunk.js"
+  },
+  {
+    "revision": "a427b0aeea9b804d467a",
+    "url": "/mans-bestfriend/static/js/main.274d2888.chunk.js"
+  },
+  {
+    "revision": "63dc7f519966b3936ab4",
+    "url": "/mans-bestfriend/static/js/runtime~main.17d8baa9.js"
+  },
+  {
+    "revision": "07b92689d134fe99bc70b8757f3081d6",
+    "url": "/mans-bestfriend/static/media/background.07b92689.jpg"
+  }
+]);
